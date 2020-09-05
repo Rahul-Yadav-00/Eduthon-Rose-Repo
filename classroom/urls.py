bfe72f57eec184a29289ada96b88d5d426b784f7
pawarharshit <49332515+pawarharshit@users.noreply.github.com>
2020-09-05 17:12:42 +0530

    Test is created

--- classroom/urls.py
+++ classroom/urls.py
@@ -4,7 +4,9 @@ from . import views
 urlpatterns = [
     path('joincls/',views.joinClass,name='joincls'),
     path('createcls/',views.createClass,name='createcls'),
-    path('classdetail/<slug:clscode>',views.classDetails,name='classdetail'),
+    path('classdetail/<slug:clscode>/',views.classDetails,name='classdetail'),
+    path('createtest/<slug:clscode>/',views.createTest,name='createtest'),
+
     #rest api
     path('all-joinclass/',views.allJoinclass,name='all-joinclass'),
     path('all-ownclass/',views.allOwnclass,name='all-ownclass')
