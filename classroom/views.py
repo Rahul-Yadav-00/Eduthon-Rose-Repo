1be3f6a04b0c927e663e674793f61f68f29874a4
pawarharshit <49332515+pawarharshit@users.noreply.github.com>
2020-09-05 13:32:41 +0530

    chat model is added and  class detail api created
    create class and join class

--- classroom/views.py
+++ classroom/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseBadRequest
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
@@ -35,3 +35,68 @@ def allOwnclass(request):
         return Response(serializer.data)
     else:   
         return HttpResponse('you not create any classroom yet')
+
+@login_required(login_url='signin')
+def joinClass(request):
+    if(request.method == 'POST'):
+        clscode = request.POST['classcode']
+        if(ClassRoom.objects.filter(classCode = clscode).exists()):
+            classroom = ClassRoom.object.get(classCode = clscode)
+            if(classroom.teacher == request.user):
+                messages(request,'you are teacher in this class')
+                return redirect('index')
+            else:
+                class_student = ClassStudents.objects.create(student=request.user,classroom = classroom)
+                messages.success(request,'class is joined')
+                return redirect('index')
+        else:
+            messages.error(request,'wrong class code')
+            return redirect('index')
+    else:
+        return HttpResponseBadRequest("error")
+
+@login_required(login_url='signin')
+def createClass(request): 
+    if(request.method == 'POST'):
+        classname = request.POST['classname']
+        classdecription = request.POST['classdescription']
+        classsubject = request.POST['classsubject']
+        classroom = ClassRoom.objects.create(classname=classname,classdecription=classdecription,
+                                                classsubject=classsubject,teacher=request.user)
+        messages.success(request,'class is created')
+        return  redirect('index')
+    else:
+        messages.error(request,'error')
+        return redirect('index')
+
+@login_required(login_url='signin')
+def classDetails(request,clscode):
+    classroom = ClassRoom.objects.get(classCode=clscode)
+    if(classroom.teacher == request.user):
+        classroom = ClassRoom.objects.get(classCode = clscode)
+        class_students =  ClassStudents.objects.none()
+        students = []
+        if(ClassStudents.objects.filter(classroom = classroom).exists()):
+            class_students = ClassStudents.objects.filter(classroom = classroom)
+            for c_s in class_students:
+                students.append(c_s.student)
+        chats = Chat.objects.none()
+        if(Chat.objects.filter(classroom = classroom).exists()):
+            chats  = Chat.objects.filter(classroom = classroom)
+
+        data = {'students':students,'chats':chats}
+        return render(request,'classroom/ownclass.html',data)
+    else:
+        classroom = ClassRoom.objects.get(classCode = clscode)
+        class_students =  ClassStudents.objects.none()
+        students = []
+        if(ClassStudents.objects.filter(classroom = classroom).exists()):
+            class_students = ClassStudents.objects.filter(classroom = classroom)
+            for c_s in class_students:
+                students.append(c_s.student)
+        chats = Chat.objects.none()
+        if(Chat.objects.filter(classroom = classroom).exists()):
+            chats  = Chat.objects.filter(classroom = classroom)
+
+        data = {'students':students,'chats':chats}
+        return render(request,'classroom/joinclass.html',data)
